dcc6215c957b87c2fae166d315a5779a8621b140
kobby08 <linusquam@gmail.com>
2022-02-19 13:27:33 +0000

    updated test for sneaker model

--- spec/factories/sneakers.rb
+++ spec/factories/sneakers.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :sneaker do
     name { 'Air Jordan' }
     model { 'Ninja Edition' }
-    description { 'MyText' }
+    description { 'This is a cool product!' }
     price { '9.99' }
     image { 'MyText' }
   end
--- spec/models/sneaker_spec.rb
+++ spec/models/sneaker_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Sneaker, type: :model do
-  context 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:model) }
-    it do
-      should(
-        validate_length_of(:description)
-          .is_at_least(10)
-          .is_at_most(250)
-          .allow_nil,
-      )
+  describe 'validations' do
+    context 'on create' do
+      it 'should have a valid factory' do
+        expect(build(:sneaker)).to be_valid
+      end
+
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:model) }
+      it { should validate_length_of(:description).is_at_least(10) }
+      it { should validate_length_of(:description).is_at_most(250) }
+      it { should validate_length_of(:description).allow_nil }
     end
   end
 end
