a53c72fec3730d872576b028d7eac4e28e38fefd
kobby08 <linusquam@gmail.com>
2022-03-15 03:34:28 +0000

    styled store page and authentication pages

--- app/helpers/sneakers_helper.rb
+++ app/helpers/sneakers_helper.rb
@@ -1,9 +1,18 @@
 module SneakersHelper
   def authentication_helper
     unless user_signed_in?
-      (link_to 'Sign In', new_user_session_path, class: "btn btn-outline-success me-2") + (link_to ' Sign Up', new_user_registration_path, class: "btn btn-success")
+      (
+        link_to 'Sign In',
+                new_user_session_path,
+                class: 'btn btn-outline-secondary me-2'
+      ) +
+        (
+          link_to ' Sign Up',
+                  new_user_registration_path,
+                  class: 'btn btn-secondary'
+        )
     else
-      button_to 'Log out', destroy_user_session_path, method: :delete, data: { turbo: 'false',}, class: "btn btn-danger"
+      render 'shared/profile_dropdown'
     end
   end
 end
